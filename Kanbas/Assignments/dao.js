c272b574d2d1477498e0b2030096772958395952
Kaden <casanave.k@northeastern.edu>
2024-11-22 11:16:39 -0500

    assignment fix

--- Kanbas/Assignments/dao.js
+++ Kanbas/Assignments/dao.js
@@ -5,6 +5,7 @@ export function findAssignmentsForCourse(courseId) {
 }
 export function createAssignment(assignment) {
     const newAssignment = { ...assignment, _id: Date.now().toString() };
+    console.log("creating assignment", newAssignment)
     Database.assignments = [...Database.assignments, newAssignment];
     return newAssignment;
 }
@@ -13,6 +14,7 @@ export function deleteAssignment(assignmentId) {
     Database.assignments = assignments.filter((assignment) => assignment._id !== assignmentId);
 }
 export function updateAssignment(assignmentId, assignmentUpdates) {
+    console.log("updating assignment", assignmentUpdates)
     const { assignments } = Database;
     const assignment = assignments.find((assignment) => assignment._id === assignmentId);
     Object.assign(assignment, assignmentUpdates);
